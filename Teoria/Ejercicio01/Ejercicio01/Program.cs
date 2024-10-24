ffd97b509f2a021f1583e5d5b16b8baf3ac26eee
Wifth <alexander.obando.os2206@gmail.com>
2024-10-24 08:20:27 -0500

    Ejercicio01 Teoria

--- Teoria/Ejercicio01/Ejercicio01/Program.cs
+++ Teoria/Ejercicio01/Ejercicio01/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Security.Cryptography;
+
+string[] edificio1 = new string[5];
+for (int x = 0; x < edificio1.Length; x++)
+{
+    Console.WriteLine($"Ingrese nombre {x + 1}: ");
+    edificio1[x] = Console.ReadLine();
+}
+Console.Clear();
+for (int x = 0; x < edificio1.Length; x++)
+{
+    Console.WriteLine(edificio1[x]);
+}
+Console.WriteLine();
+string[] edificio2 = { "Tú", "Papás", "Hermano", "Tío", "Primo" };
+foreach (string e in edificio2)
+{
+    Console.WriteLine(e);
+}
