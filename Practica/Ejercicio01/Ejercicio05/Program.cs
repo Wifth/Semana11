76efae695f9ffbd4c6a76e982cd8a67a8b6d56cd
Wifth <alexander.obando.os2206@gmail.com>
2024-10-24 12:18:32 -0500

    Ejercicio6 Practica

--- Practica/Ejercicio01/Ejercicio05/Program.cs
+++ Practica/Ejercicio01/Ejercicio05/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+char[] nom = { 'a', 'e', 'i', 'o', 'u' };
+Console.WriteLine("Lista original: ");
+foreach (char c in nom) 
+    Console.WriteLine(c);
+    Console.WriteLine();
+
+Console.Write("Ingresar el caracter a modificar: ");
+char modificar = char.Parse(Console.ReadLine());
+
+int indice = Array.IndexOf(nom, modificar);
+
+if (indice != -1) 
+{
+    Console.Write("Ingrese un nuevo caracter: ");
+    char b = char.Parse(Console.ReadLine());
+    Console.WriteLine("\nLista modificada: ");
+    nom[indice] = modificar;
+    foreach (char c in nom)
+        Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine("\nNo se puede modificar, no existe");
+}
+
+
+
---
+++ Practica/Ejercicio01/Ejercicio06/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] num = new int[0];
+int posicion = 0, datos;
+char t;
+
+Console.Write("Ingresar el número: ");
+do
+{
+
+    while (!int.TryParse(Console.ReadLine(), out datos))
+    {
+        Console.Write("Error: Ingrese número: ");
+    }
+    Array.Resize(ref num, num.Length + 1);
+    num[posicion] = datos;
+    posicion++;
+
+    do
+    {
+        Console.Write("\nDesea seguir registrando (s/n): ");
+        t = char.ToLower(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+    } while (t != 's' && t != 'n');
+
+} while (t == 's');
+Console.WriteLine("\nLista de números registrados: ");
+foreach(int i in num)
+Console.WriteLine(i);
