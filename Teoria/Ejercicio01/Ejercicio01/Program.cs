59332445794f70ed9e32e01fdd346a6523fab61b
Wifth <alexander.obando.os2206@gmail.com>
2024-10-24 08:58:29 -0500

    Ejercicio01 Teoria

--- Teoria/Ejercicio01/Ejercicio01/Program.cs
+++ Teoria/Ejercicio01/Ejercicio01/Program.cs
@@ -14,8 +14,18 @@ for (int x = 0; x < edificio1.Length; x++)
     Console.WriteLine(edificio1[x]);
 }
 Console.WriteLine();
-string[] edificio2 = { "Tú", "Papás", "Hermano", "Tío", "Primo" };
+string[] edificio2 = { "tu", "papas", "hermano", "tio", "primo" };
 foreach (string e in edificio2)
 {
     Console.WriteLine(e);
+}
+Console.WriteLine("\n¿A quién busca en edificio 2?: ");
+string bus = Console.ReadLine().ToLower();
+for (int x = 0; x < edificio2.Length; x++)
+{
+    if (edificio2[x] == bus)
+    {
+        Console.Write($"\nEstá en el piso {x + 1}");
+        break;
+    }
 }
